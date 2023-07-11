7db7ef207688024d4154e6e560ae9c6010b12d81
Muneer Khalid <muneer.ansari.2646@gmail.com>
2023-07-11 13:13:55 +0500

    React-native start

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+
+  return (
+    <View style={{flexDirection: 'row', gap: 10}}>
+      <Text>Thankgod</Text>
+    </View>
+  ) 
+}
+
+export default Home;
