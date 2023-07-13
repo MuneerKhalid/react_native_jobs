7014d711c983a604a672c2d1eb629360bfd1f0a8
Muneer Khalid <muneer.ansari.2646@gmail.com>
2023-07-13 22:55:17 +0500

    Popularjobs and api fetch

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -5,12 +5,16 @@ import { useRouter } from 'expo-router';
 import styles from './popularjobs.style';
 import { COLORS, SIZES } from '../../../constants';
 import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import useFetch from '../../../hook/useFetch';
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
-  
+
+  const { data, isLoading, error } = useFetch('search', {
+    query: 'React Developer',
+    num_pages: 1
+  })
+  // console.log(data);
 
   return (
     <View style={styles.container}>
@@ -28,7 +32,17 @@ const Popularjobs = () => {
         ) : error ? (
           <Text>Oops! Something went wrong</Text>
         ) : (
-          <FlatList />
+          <FlatList 
+            data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+            renderItem={({ item }) => (
+              <PopularJobCard 
+                item = {item}
+              />
+            )}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
         )}
       </View>
     </View>
